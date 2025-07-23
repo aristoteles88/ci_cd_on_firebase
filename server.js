124d0964e83126ccf28f08437c78ecde60c0b47c
aristoteles88 <aristoteles88@gmail.com>
2025-07-23 17:02:07 -0300

    Update NodeJS app

--- server.js
+++ server.js
@@ -1,26 +1,10 @@
 require('dotenv').config();
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyA5E_97lqAvIgcNUAYav2Qlnz8T06fuJfo",
-  authDomain: "ci-cd-github-firebase.firebaseapp.com",
-  projectId: "ci-cd-github-firebase",
-  storageBucket: "ci-cd-github-firebase.firebasestorage.app",
-  messagingSenderId: "1094992314811",
-  appId: "1:1094992314811:web:b10e8da1381d278dacc49e"
-};
-
 const express = require('express');
 const path = require('path');
 
 
 const app = express();
-app.initializeApp(firebaseConfig)
 const port = process.env.PORT || 3000;
 
 // Middleware
